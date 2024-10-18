10af5ae35d84a9c7682865452fa8e86ddbbe1fc1
tenzinpalden <tpalden2003@gmail.com>
2024-10-18 16:58:01 -0400

    created homepage and setup routes

---
+++ frontend/src/pages/homepage.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+  return (
+    <div className="homepage-container">
+      {/* Header Section */}
+      <header className="header">
+        <h1 className="title">Technique Tree</h1>
+        <div className="logo">Logo</div>
+      </header>
+
+      {/* Description Section */}
+      <section className="description-section">
+        <p className="description">
+          This is a description paragraph that gives an overview of Technique Tree. You can put your main content here.
+        </p>
+      </section>
+
+      {/* Circular Buttons Section */}
+      <section className="buttons-section">
+        <div className="button">
+          <span>View Content</span>
+        </div>
+        <div className="button">
+          <span>Create Account</span>
+        </div>
+        <div className="button">
+          <span>Maybe Other</span>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
