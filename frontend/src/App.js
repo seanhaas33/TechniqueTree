1fc1743e980aa7e8ae720786e837578c3b145217
tenzinpalden <tpalden2003@gmail.com>
2024-10-18 17:09:45 -0400

    created simple navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-//import Navbar from './components/navbar';
+import Navbar from './components/navbar';
 import HomePage from './pages/homepage';
 
 function App() {
   return (
     <Router>
-      {/* <Navbar />  */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />  {/* Correct usage of element */}
       </Routes>
