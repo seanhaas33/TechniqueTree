725e8a33176e05de7041fae3e52ea2328de9b18b
tenzinpalden <tpalden2003@gmail.com>
2024-10-20 17:45:47 -0400

    added homepage description and updated css

--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -8,7 +8,17 @@ const HomePage = () => {
       {/* Description Section */}
       <section className="description-section">
         <p className="description">
-          This is a description paragraph that gives an overview of Technique Tree. You can put your main content here.
+        Technique Tree aims to change how athletes and coaches learn about their sport. 
+        With no financial or geographical limitations, information will be accessible to all. 
+        With its intuitive layout, it outperforms current video sharing platforms. 
+        The tree-like display demonstrates which knowledge is essential for one's sport, and what 
+        they can subsequently learn. Users can see and evaluate their progress in various areas of 
+        their sport. A rating system, comments, and video replies will allow users to give their input 
+        and allow demonstrators to enhance users’ understanding of their content. 
+        Top athletes and coaches will have their own exclusive series. This could start with RPI athletes 
+        and expand as the user base grows. The site will start with specific athletes, and eventually, 
+        anybody will be able to create their own series. Whether coaches want to format their teachings to 
+        their team, or any individual wants their knowledge shared, it will be possible through Technique Tree.
         </p>
       </section>
 
