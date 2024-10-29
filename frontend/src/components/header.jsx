664f42b572bac20635a2dd610d195699dbbf2b8d
tenzinpalden <tpalden2003@gmail.com>
2024-10-29 16:48:13 -0400

    uploaded logo images

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -5,10 +5,11 @@ import './header.css';
 const Header = () => {
   return (
     <header className="header-container">
-         <h1 className="title">Technique Tree</h1>
-        <div className="logo">Logo</div>
+      <h1 className="title">Technique Tree</h1>
+      <div className="logo">
+        <img src="/logo_clear.png" alt="Logo" className="logo-image" />
+      </div>
     </header>
-    
   );
 };
 
